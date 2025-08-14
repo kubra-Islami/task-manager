651cfc42699f670b5608fb854e528d2344112f04
kubra islami <kubra.islami.00@gmail.com>
2025-08-14 12:40:37 +0100

    the dist folder as an outputDirectory was written in vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  // base: '/', // default; keep it if you previously changed it
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
